c84fee7891753e90e13256c9028c4960b1a1c1b1
Nicat Isayev <nicat.isayev@hotmail.com>
2018-03-07 11:11:08 +0400

    Add project files.

---
+++ WpfApp3/AddPerson.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for AddPerson.xaml
+    /// </summary>
+    public partial class AddPerson : Window
+    {
+        Person person;
+        public AddPerson()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            person = new Person
+            {
+                Name = textboxName.Text,
+                Surname = textboxSurname.Text,
+                ImagePath = textboxImagePath.Text
+            };
+
+            App.list.Add(person);
+            Hide();
+
+        }
+    }
+}
---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            listboxPerson.ItemsSource = App.list;
+        }
+        
+        private void Addbutton_Click(object sender, RoutedEventArgs e)
+        {
+            AddPerson addPerson = new AddPerson();
+            addPerson.Owner = this;
+            addPerson.Show();
+        }
+    }
+}
